d0e1c5b725be4cfbbcc7c743ee5cfbf5ff0ef205
Andrew <iglooworks1505@gmail.com>
2020-06-03 15:49:55 -0500

    edited about me section

--- client/src/pages/AboutMe.js
+++ client/src/pages/AboutMe.js
@@ -23,7 +23,7 @@ const AboutMe = (props) => {
           the course I looked forward to the least that year. That following
           summer I ended up breaking my ankle, basically leaving me locked in my
           apartment for 3 months with nothing to do. I started to play around
-          with different development technologies (Python originally), and
+          with different development technologies, Python originally, and
           immediately became hooked.
         </p>
         <div style={{ height: 8 }}></div>
@@ -31,26 +31,20 @@ const AboutMe = (props) => {
           From there I spent endless hours reading, watching tutorials, and
           experimenting with different technologies and tools. This led to
           Javascript, and from there to React and Node.js. Even though
-          Javascript is still primarily a front-end technology, I love the logic
-          and problem solving inherent to backend work. That all left me with my
-          latest undertaking, working towards a Java certification and hopefully
-          in the near future being able to comfortably work with frameworks such
-          as Spring.
+          Javascript is still primarily a client side technology, I love the
+          logic and problem solving inherent to backend work. This all leads up
+          to my latest endeavor, attempting to get a Oracle Java certification
+          this summer and hopefully in the near future being able to comfortably
+          work with frameworks such as Spring.
         </p>
         <div style={{ height: 8 }}></div>
         <p className="mobileTextAdjust">
           What I love most about development is that there never comes a point
           in which you know everything. I've always loved learning. Whether
-          that's Physics and cosmology, ancient history, or anything in between,
-          I've always had a need to figure out how and why things work the way
-          they do. I spent three hours reading about baby kangaroos this past
-          weekend. An animal I'll almost surely never see, just because I found
-          them fascinating{" "}
-          <em>
-            (Did you know they're only 2cm tall, and weigh less than a gram when
-            they're born?!?!)
-          </em>
-          .
+          that's trying to figure out the mysteries of the universe and
+          existence itself, or spending 3 hours researching baby kangaroos... an
+          animal I'll almost surely never see, just because I read a short
+          article that fascinated me.
         </p>
       </div>
       <TechIcons />
